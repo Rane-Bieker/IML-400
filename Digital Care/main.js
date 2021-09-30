137c36d1da5e92ce94927f4d0908a2e8e8c9308b
Rane <rbieker@usc.edu>
2021-09-30 12:51:20 -0700

    changable bg

--- Digital Care/main.js
+++ Digital Care/main.js
@@ -1,5 +1,35 @@
 $(document).ready(function () {
+  $("#shark").fadeIn(1000);
+
   $(function () {
     $(".drag").draggable();
+  });
+
+  $("#shk").click(function () {
+    $("#camp").fadeOut(1000);
+    $("#ruble").fadeOut(1000);
+    $("#bird").fadeOut(1000);
+    $("#shark").fadeIn(1000);
+  });
+
+  $("#cat").click(function () {
+    $("#camp").fadeOut(1000);
+    $("#shark").fadeOut(1000);
+    $("#bird").fadeOut(1000);
+    $("#ruble").fadeIn(1000);
+  });
+
+  $("#cmp").click(function () {
+    $("#ruble").fadeOut(1000);
+    $("#shark").fadeOut(1000);
+    $("#bird").fadeOut(1000);
+    $("#camp").fadeIn(1000);
+  });
+
+  $("#brd").click(function () {
+    $("#camp").fadeOut(1000);
+    $("#shark").fadeOut(1000);
+    $("#ruble").fadeOut(1000);
+    $("#bird").fadeIn(1000);
   });
 });
